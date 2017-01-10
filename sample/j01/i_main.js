bf8b2e76fe91a5f5cbde7063d6c95eb2aa1199da
Hayato Izumi <hayto-i@digirea.com>
2017-01-10 19:18:30 +0900

    @上から順に読み込んでいるので、動的にIDを取得できるようにしなければならない。

--- sample/j01/i_main.js
+++ sample/j01/i_main.js
@@ -1,9 +1,11 @@
 
 // 初期化関数でも扱えるようにグローバルスコープ
 var x = 0;
-// ゲーム判定処理用配列
+
+// 判定処理用配列
 var cell = [0,0,0,0,0,0,0,0,0];
 var cellId = ['s0','s1','s2','s3','s4','s5','s6','s7','s8'];
+
 /* 初期化関数を書きたかった
 function init(){
     var i = 0;
@@ -32,34 +34,48 @@ function winJudge(){
     }
 }
 */
-
 window.onload = function main(){
 
-    //テスト用関数
-    //function myFunc() {alert('ckick event!');}
+    // テスト用関数
+    // function myFunc() {alert('ckick event!');}
 
-    //偶数回のクリックは○、奇数回は×を描画
-    //すでに要素があるセルには上書きできない処理をしたい
-   
-    if(x%2==0 && cell[0]==0){
-        //div範囲のID取得
-        var element = document.getElementById('s0');
-        //id = s0の挙動確認
-        element.addEventListener('click', function drawCircle(eve){
-            eve.currentTarget.textContent = "○";
-        }, false);
-        cell[0] = 1;
-        x+=1;
-    }else{
-        //div範囲のID取得
-        var element = document.getElementById('s0');
-        //id = s0の挙動確認
-        element.addEventListener('click', function drawCross(eve){
-            eve.currentTarget.textContent = "×";
-        }, false);
-        x+=1;
-        cell[0] = -1;
-    }
+    // 偶数回のクリックは○、奇数回は×を描画
+    // すでに要素があるセルには上書きできない処理を追記
 
+    //
     
+        if(x%2==0 && cell[0] == 0){
+            var element = document.getElementById('s0')
+            element.addEventListener('click', function drawCircle(eve){
+                eve.currentTarget.textContent = "○";
+            }, false);
+            x+=1;
+            cell[0] = 1;// 判定用
+        }else if(x%2==1 && cell[0] == 0){
+            var element = document.getElementById('s0')
+            element.addEventListener('click', function drawCross(eve){
+                eve.currentTarget.textContent = "×";
+            }, false);
+            x+=1;
+            cell[0] = -1;// 判定用
+        }
+
+        if(x%2==0 && cell[6] == 0){
+            var element = document.getElementById('s6')
+            element.addEventListener('click', function drawCircle(eve){
+                eve.currentTarget.textContent = "○";
+            }, false);
+            x+=1;
+            cell[6] = 1;// 判定用
+        }else if(x%2==1 && cell[6] == 0){
+            var element = document.getElementById('s6')
+            element.addEventListener('click', function drawCross(eve){
+                eve.currentTarget.textContent = "×";
+            }, false);
+            x+=1;
+            cell[6] = -1;// 判定用
+        }
+
+
 }
+
