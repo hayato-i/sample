dc6ecce0d6d4ce0b9a0dcd91778fb88ab9d06b8f
Hayato Izumi <hayto-i@digirea.com>
2017-01-10 12:56:35 +0900

    @子要素の追加の必要がないためappendChildの実装をなくした。または、divを配置せずに新しく生成するという手もあるかもしれない。

--- sample/j01/i_main.js
+++ sample/j01/i_main.js
@@ -15,37 +15,23 @@
     }   
 */
 
+//何回目の描画か保持する変数
+var x = 0;
+
 window.onload = function (){
+    //描画回数更新
+    x++;
 
     //テスト用関数
     //function myFunc() {alert('ckick event!');}
-    
-    //描画関数
-    function drawCircle(eve){
-        var newElement = document.createElement('h1');
-        eve.appendChild(newElement);
-        newElement.textContent = "○";
-    }
 
     //div範囲のID取得
     var element = document.getElementById('s0');
     //id = s0の挙動確認
-    element.addEventListener('click', drawCircle(element), false);
+    element.addEventListener('click', function drawCircle(eve){
+        eve.textContent = "○";
+    }, false);
     
+    // 
+    // requestAnimationFrame(function);
 }
-
-/*    
-    //初期化 リセット時には入力記号も削除する？
-    window.onload = init(){
-
-        //クリック回数初期化
-        var clickNum = 0;
-        var box = {0,0,0,0,0,0,0,0,0};
-
-        //文字列リセット
-        var i = 0;
-        for(i=0;i<9;i++){
-
-        }
-    }   
-*/
