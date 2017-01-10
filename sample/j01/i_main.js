5932726cacc37beb64d183fbce96249e061f36b7
Hayato Izumi <hayto-i@digirea.com>
2017-01-10 10:48:35 +0900

    @丸の描画が初期状態で呼び出されてしまっている。

--- sample/j01/i_main.js
+++ sample/j01/i_main.js
@@ -1,9 +1,7 @@
-//function(){
-    /*
-    var box = {0,0,0,0,0,0,0,0,0};
-    
-    //初期化 リセット時には入力記号も削除する
-    init(){
+
+/*    
+    //初期化 リセット時には入力記号も削除する？
+    window.onload = init(){
 
         //クリック回数初期化
         var clickNum = 0;
@@ -14,24 +12,40 @@
         for(i=0;i<9;i++){
 
         }
+    }   
+*/
+
+window.onload = function (){
+
+    //テスト用関数
+    //function myFunc() {alert('ckick event!');}
+    
+    //描画関数
+    function drawCircle(eve){
+        var newElement = document.createElement('h1');
+        eve.appendChild(newElement);
+        newElement.textContent = "○";
     }
-    */
 
-    window.addEventListener('load',function (){
-        //子要素としての'○'の描画
+    //div範囲のID取得
+    var element = document.getElementById('s0');
+    //id = s0の挙動確認
+    element.addEventListener('click', drawCircle(element), false);
+    
+}
 
-        //div範囲のID取得
-        var element = document.getElementById('square0');
+/*    
+    //初期化 リセット時には入力記号も削除する？
+    window.onload = init(){
 
-        
-        element.addEventListener('click', drawCircle(eve) = { 
-            eve.createElement('h1');
-            eve.appendChild(newElement);
-            eve.textContent = '○';
-        }, false);
+        //クリック回数初期化
+        var clickNum = 0;
+        var box = {0,0,0,0,0,0,0,0,0};
 
-    },false);
-        
+        //文字列リセット
+        var i = 0;
+        for(i=0;i<9;i++){
 
-    //}   
-//}
+        }
+    }   
+*/
