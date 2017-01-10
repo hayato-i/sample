1625c88fd1617cb7d2dec47f09c045d35991fc51
Hayato Izumi <hayto-i@digirea.com>
2017-01-10 17:02:41 +0900

    @初期化の関数を書きたかったが断念。○×の表示実装に戻る。

--- sample/j01/i_main.js
+++ sample/j01/i_main.js
@@ -1,37 +1,37 @@
-
-/*    
-    //初期化 リセット時には入力記号も削除する？
-    window.onload = init(){
-
-        //クリック回数初期化
-        var clickNum = 0;
-        var box = {0,0,0,0,0,0,0,0,0};
-
-        //文字列リセット
-        var i = 0;
-        for(i=0;i<9;i++){
-
-        }
-    }   
-*/
-
-//何回目の描画か保持する変数
 var x = 0;
+/* 初期化関数を書きたかった
+var cell = [0,0,0,0,0,0,0,0,0];
 
-window.onload = function (){
-    //描画回数更新
-    x++;
+function init(){
+    var i = 0;
+    for(i=0;i<9;i++){
+        cell[i] = 0;
+
+    }
+}
+*/
+window.onload = function main(){
 
     //テスト用関数
     //function myFunc() {alert('ckick event!');}
 
-    //div範囲のID取得
-    var element = document.getElementById('s0');
-    //id = s0の挙動確認
-    element.addEventListener('click', function(eve){
-        eve.currentTarget.textContent = "○";
-    }, false);
-    
-    // 
-    // requestAnimationFrame(function);
+    if(x%2==0){
+        //div範囲のID取得
+        var element = document.getElementById('s0');
+        //id = s0の挙動確認
+        element.addEventListener('click', function(eve){
+            eve.currentTarget.textContent = "×";
+            x++;
+        }, false);
+    }else{
+        //div範囲のID取得
+        var element = document.getElementById('s0');
+        //id = s0の挙動確認
+        element.addEventListener('click', function(eve){
+            eve.currentTarget.textContent = "○";
+            x++;
+        }, false);
+    }
+
+    requestAnimationFrame(main);
 }
