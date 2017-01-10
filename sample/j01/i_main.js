351743b890874c83b4da184a7049aa01cd7ca481
Hayato Izumi <hayto-i@digirea.com>
2017-01-10 17:23:00 +0900

    @requastAnimationを使うとインクリメントが正常に作動しない。

--- sample/j01/i_main.js
+++ sample/j01/i_main.js
@@ -1,4 +1,5 @@
 var x = 0;
+
 /* 初期化関数を書きたかった
 var cell = [0,0,0,0,0,0,0,0,0];
 
@@ -10,28 +11,30 @@ function init(){
     }
 }
 */
+
 window.onload = function main(){
 
     //テスト用関数
     //function myFunc() {alert('ckick event!');}
 
+    //偶数回のクリックは○、奇数回は×を描画
+    //すでに要素があるセルには上書きできない処理をしたい
     if(x%2==0){
         //div範囲のID取得
         var element = document.getElementById('s0');
         //id = s0の挙動確認
-        element.addEventListener('click', function(eve){
-            eve.currentTarget.textContent = "×";
-            x++;
+        element.addEventListener('click', function drawCircle(eve){
+            eve.currentTarget.textContent = "○";
         }, false);
+        x+=1;
     }else{
         //div範囲のID取得
         var element = document.getElementById('s0');
         //id = s0の挙動確認
-        element.addEventListener('click', function(eve){
-            eve.currentTarget.textContent = "○";
-            x++;
+        element.addEventListener('click', function drawCross(eve){
+            eve.currentTarget.textContent = "×";
         }, false);
+        x+=1;
     }
 
-    requestAnimationFrame(main);
 }
