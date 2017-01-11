1c48dd90c23399f85d4c1e484427c950e11c6df6
Hayato Izumi <hayto-i@digirea.com>
2017-01-11 18:50:02 +0900

    @一気に更新してしまった。IDによるブラシサイズや色の変化の実装を始めた

--- sample/j03/i_main.js
+++ sample/j03/i_main.js
@@ -1,27 +1,38 @@
 
 (function(){
-    var canvas, layer;
+    var canvas, layer, brushC, bruchS;
     var ctx, lctx;
     var mousePosition = new Position(0, 0);
+    var mDownFlag = false;
+    var brushColor = 'red';
+    var brushSize = 10;
 
     window.addEventListener('load', function(){
         canvas = document.getElementById('canvas');
         canvas.addEventListener('mousemove', mouseMove, false);
+        canvas.addEventListener('mousedown', mouseDown, false);
+        canvas.addEventListener('mouseup',mouseUp,false);
+        canvas.addEventListener('init',init,false);
+
+        // 筆のパラメータ
+        
+        brushC = document.getElementById('brushSize');
+
         canvas.width = 700;
         canvas.height = 500;
         ctx = canvas.getContext('2d');
         console.log(ctx);
-
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = brushColor;
     }, false);
 
     function drawCircle(pos){
         if(!ctx){return;}
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.beginPath();
-        ctx.arc(pos.x, pos.y, 10, 0, Math.PI * 2);
-        ctx.closePath();
-        ctx.fill();
+        if(mDownFlag === true){
+            ctx.beginPath();
+            ctx.arc(pos.x, pos.y, brushSize, 0, Math.PI * 2);
+            ctx.closePath();
+            ctx.fill();
+        }
     }
 
     function mouseMove(eve){
@@ -32,8 +43,21 @@
         drawCircle(mousePosition);
     }
 
+    function mouseDown(){
+        mDownFlag = true;
+    }
+
+    function mouseUp(){
+        mDownFlag = false;
+    }
+
+    function init(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
     function Position(x, y){
         this.x = x;
         this.y = y;
     }
+
 })();
