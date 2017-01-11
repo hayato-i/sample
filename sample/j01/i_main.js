6b8dcab6c0eb8124529b7b471c4e0e99aef78af2
Hayato Izumi <hayto-i@digirea.com>
2017-01-11 17:14:18 +0900

    @お絵かきソフトっぽくしたい

--- sample/j01/i_main.js
+++ sample/j01/i_main.js
@@ -1,6 +1,12 @@
 
 
+// クリック回数保持用
+var x = 0;
+// 判定処理用配列
+var cell = [0,0,0,0,0,0,0,0,0];
+var cellId = ['s0','s1','s2','s3','s4','s5','s6','s7','s8'];
 
+/*
 function init(){
     var i = 0;
     for(i=0;i<9;i++){
@@ -12,10 +18,10 @@ function init(){
     // ループですべてのIDの要素を消したかった
 
 }
+*/
 
-/*
 function winJudge(){
-    // 8列どこかが○だったら
+    // ○が1列揃ったら
     if(cell[0]+cell[1]+cell[2]==3 || 
        cell[3]+cell[4]+cell[5]==3 ||
        cell[6]+cell[7]+cell[8]==3 ||
@@ -27,34 +33,34 @@ function winJudge(){
         alert('○ is winner!');
         return 1;
     }
-    // どこかが×だったら
-    else if(){
+    // ×が1列揃ったら
+    else if(cell[0]+cell[1]+cell[2]==-3 || 
+       cell[3]+cell[4]+cell[5]==-3 ||
+       cell[6]+cell[7]+cell[8]==-3 ||
+       cell[0]+cell[3]+cell[6]==-3 ||
+       cell[1]+cell[4]+cell[7]==-3 ||
+       cell[2]+cell[5]+cell[8]==-3 ||
+       cell[0]+cell[4]+cell[8]==-3 ||
+       cell[2]+cell[4]+cell[6]==-3){
         alert('× is winner!');
-        return 1;
+        return;
     }
     // 全部うまったら
-    else if(x==8){
+    else if(x==9){
         alert('draw game');
         return 1;
     }
 }
-*/
 
-// クリック回数保持用
-var x = 0;
-// 判定処理用配列
-var cell = [0,0,0,0,0,0,0,0,0];
-var cellId = ['s0','s1','s2','s3','s4','s5','s6','s7','s8'];
 
 // main
 window.onload = function main(){
 
     // テスト用関数
     // function myFunc() {alert('ckick event!');}
-
-
     
     var i = 0;
+    var j = 0;
     for(i=0;i<cellId.length;i++){
         // idの取得
         var element = document.getElementById(cellId[i]);
@@ -63,13 +69,23 @@ window.onload = function main(){
             if(x%2==0 && eve.currentTarget.textContent == ""){
                 eve.currentTarget.textContent = "○";
                 x+=1;
-                cell[i]=1;
+                // ここまでは大丈夫。
+                // 判定用配列に正しく数値が追加されていないので、Idの照合する
+                for(j=0;j<cellId.length;j++){
+                    if(cellId[j] == eve.currentTarget.id){
+                        cell[j]=1;
+                    }
+                }
             }
             // null かつ奇数
             else if(x%2==1 && eve.currentTarget.textContent == ""){
                 eve.currentTarget.textContent = "×"
                 x+=1;
-                cell[i]=-1;
+                for(j=0;j<cellId.length;j++){
+                    if(cellId[j] == eve.currentTarget.id){
+                        cell[j]=-1;
+                    }
+                }
             }
             winJudge();   
         }, false);
